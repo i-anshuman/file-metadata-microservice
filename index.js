3ae7bc03a9fc2cac9b9ad27dd122b6077c80eb73
Anshuman Gupta <anshuman844@gmail.com>
2020-03-14 19:10:45 +0530

    Created and served homepage.   - Added file index.html   - Styles homepage with styles.css   - Served homepage.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("file metadata microservice");
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static(__dirname + "/assets/"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
